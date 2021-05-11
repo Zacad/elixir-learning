00246394bb39184388773c7a52b19fe6fa73009b
Adam Z <13Szary13!>
2021-05-11 19:35:49 +0200

    add functions

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -15,4 +15,24 @@ defmodule Cards do
   def hello do
     :world
   end
+
+  def create_deck do
+    values = ["ace", "two", "three", "four", "five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
 end
