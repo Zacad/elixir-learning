d742f74d9b36d050a197c316e8b17f17f3d3769e
Zacad <az@adamzajac.info>
2021-05-16 16:12:09 +0200

    identicon init

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -32,7 +32,33 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Some module documentation
+
+  ## Examples
+
+      some examples
+
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      {:ok, deck} -> :erlang.binary_to_term(deck)
+      {:error, _reason} -> "Something went wrong"
+    end
+  end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle()
+    |> Cards.deal(hand_size)
   end
 end
